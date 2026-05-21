2804529f56bec3972b59e2e1b75a857948e551e6
Sanchitwakde <wakdes70@gmail.com>
2026-05-21 13:43:06 +0530

    day 9

--- Day 9/graph.py
+++ Day 9/graph.py
@@ -3,21 +3,51 @@
 # also there are directed + unweighted, weighted + undirected , undirected + unweighted ,undirected + weighted
 import sys
 class Graph:
-    def __init__():
-        pass
-    def addnode():
-        pass
-    def addedge_undirected_unweighted():
-        pass
+    def __init__(self):
+        self.nodes=[]
+        self.nodecount=0
+        self.graph=[]
+    def addnode(self,v):
+        if v in self.nodes:
+            print(v,"is already present")
+        else:
+            self.nodecount+=1
+            self.nodes.append(v)
+            for x in self.graph:
+                x.append(0)
+            temp =[]
+            for x in range(self.nodecount):
+                temp.append(0)
+            self.graph.append(temp)
+            print(v,"is added")
+
+    def addedge_undirected_unweighted(self,v1,v2):
+        if v1 not in self.nodes:
+            print(v1, " not present ")
+            return
+        if v2 not in self.nodes:
+            print(v2, " not present ")
+            return
+        index1 = self.nodes.index(v1)
+        index2 = self.nodes.index(v2)
+        self.graph[index1][index2]=1
+        self.graph[index2][index1]=1
+
     def addedge_undirected_weighted():
         pass
     def addedge_directed_weighted():
         pass
-    def printgraph():
-        pass
+    def printgraph(self):
+        print(" ",*self.nodes)
+        for i in range(self.nodecount):
+            print(self.nodes[i]," ")
+            for j in range(self.nodecount):
+                print(self.graph[i][j],end = " ")
+            print()
+
     def deletegraph():
         pass
-    
+
 if __name__ == '__main__':
     obj = Graph()
     while True:
@@ -30,9 +60,12 @@ if __name__ == '__main__':
         print("0. Exit\n")
         n=int(input("Enter any choice: "))
         if n==1:
-            obj.addnode()
+            v = input("Enter vertex: ")
+            obj.addnode(v)
         elif n==2:
-            obj.addedge_undirected_unweighted()
+            v1=input("Enter Vertex 1")
+            v2 = input("Enter Vertex 2")
+            obj.addedge_undirected_unweighted(v1,v2)
         elif n==3:
             obj.addedge_undirected_weighted()
         elif n==4:
